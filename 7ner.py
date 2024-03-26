5faace95c56b4f79ccc4cc1bc5c01d3dde8b0026
Carmen Brando <cvbrandoe@users.noreply.github.com>
2024-03-26 14:43:21 +0100

    Delete 7ner.py

--- 7ner.py
+++
@@ -1,9 +0,0 @@
-import spacy
-
-nlp = spacy.load("fr_core_news_sm")
-#nlp = spacy.load("fr_core_news_md")
-with open ("balzac.txt", "r") as myfile:
-    data=myfile.read()
-doc = nlp(data)
-for ent in doc.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
